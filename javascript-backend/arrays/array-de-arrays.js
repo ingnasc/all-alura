2fa59fbe990afe76cdbdf74f9626b9e3f2234d98
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-14 17:02:54 -0300

    atividade

---
+++ javascript-backend/arrays/array-de-arrays.js
@@ -0,0 +1,11 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias];
+
+console.log(lista);
+
+console.log(
+    `A aluna na posição 1 da lista é: ${lista[0][1]},
+    e sua nota foi ${lista[1][1]}`
+);
