7beb00a246cf38fdc627f0b479a285a6a1ce2e4a
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-14 17:41:36 -0300

    exercicios

---
+++ javascript-backend/arrays/exercicio6.js
@@ -0,0 +1,8 @@
+//1 - Crie uma função que receba dois arrays e os concatene em um único array.
+
+const lista1 = ['ana', 'jose'];
+const lista2 = ['joao', 'luana'];
+
+const listas = lista1.concat(lista2);
+
+console.log(listas);
