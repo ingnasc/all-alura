051d61474b4523f448266b40906e2b5b4fed2efe
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-14 16:36:37 -0300

    exercicios

---
+++ javascript-backend/arrays/exercicio2.js
@@ -0,0 +1,9 @@
+//Crie um programa em Node.js que inicie com um array cinco elementos e atribua um novo valor para a primeira posição (índice 0) desse array. Em seguida, exiba o array antes e depois da alteração.
+
+const arrayExemplo = [10, 20, 30, 40, 50];
+
+console.log('Array antes de alterar: ', arrayExemplo);
+
+arrayExemplo[0] = 9;
+
+console.log('Array depois de alterar: ', arrayExemplo);
---
+++ javascript-backend/arrays/exercicio4.js
@@ -0,0 +1,7 @@
+//Desenvolva um programa em Node.js que crie um array vazio e atribua valores a ele utilizando o método push(). Adicione três números inteiros ao array e, em seguida, exiba o array resultante.
+
+const arrayExemplo = [];
+
+arrayExemplo.push(20, 50, 60);
+
+console.log(arrayExemplo);
