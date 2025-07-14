6b5e4431beb411ffdba445e321b4b25a6ef32a67
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-14 16:53:32 -0300

    atividade

---
+++ javascript-backend/arrays/atualizar-lista.js
@@ -0,0 +1,6 @@
+const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+listaEstudantes.splice(1, 2, 'Rodrigo'); //inicio, quantidade de exclusao, adicionado antes do inicio determinado
+
+console.log(listaEstudantes);
+
