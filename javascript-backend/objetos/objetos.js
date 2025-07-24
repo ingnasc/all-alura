e2fdedeb34b312f39bd68217763dbe0e0530e59a
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-24 08:36:08 -0300

    atividade aula

---
+++ javascript-backend/objetos/objetos.js
@@ -0,0 +1,11 @@
+const listaCPFs = ['12312312312', '123234435345'];
+
+const infosPessoa = ['nome', 'Ingrid Nascimento', 'idade', 32];
+console.log(infosPessoa[1]);
+
+const objPessoa = {
+    nome: 'Ingrid Nascimento',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript'
+}
