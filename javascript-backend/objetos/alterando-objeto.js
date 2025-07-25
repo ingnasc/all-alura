c74b8f23b4b44ee9bcbf42913d058701297c0607
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-25 08:56:20 -0300

    atividade da aula

---
+++ javascript-backend/objetos/alterando-objeto.js
@@ -0,0 +1,14 @@
+const estudante = {
+    nome: 'Ingrid Nascimento',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript'
+}
+
+estudante.telefone = '5575999999999';
+
+console.log(estudante.telefone);
+console.log(estudante);
+
+estudante.nome = 'Ingrid Alves';
+console.log(estudante);
