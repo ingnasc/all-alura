0d1a0c95960bee1709323a01ffce8cf1b9013559
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-28 08:40:28 -0300

    atividade da aula

---
+++ javascript-backend/objetos/dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript',
+  bolsista: true,
+  telefones: ['5575999999999', '5575999999998'],
+}
+
+console.log(estudante.telefones[0]);
