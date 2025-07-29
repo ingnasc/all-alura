63967411d22bbd40de2727c8452c5402beb8cca7
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-29 08:31:02 -0300

    atividade da aula

---
+++ javascript-backend/objetos/objetos-aninhados.js
@@ -0,0 +1,16 @@
+const estudante = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript',
+  bolsista: true,
+  telefones: ['5575999999999', '5575999999998'],
+}
+
+estudante.endereco = {
+    rua: 'Rua Joao Maria',
+    numero: '45A',
+    complemento: 'APTO 54'
+}
+
+console.log(estudante);
