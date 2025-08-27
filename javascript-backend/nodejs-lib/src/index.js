a8434d9e49b52550690f1cd990c595da6b7cab91
Ingrid Nascimento <ingcnasc@gmail.com>
2025-08-27 06:51:14 -0300

    atividade da aula

--- javascript-backend/nodejs-lib/src/index.js
+++ javascript-backend/nodejs-lib/src/index.js
@@ -1,4 +1,22 @@
+const fs = require('fs');
+
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
-console.log(link);
+//como é um arquivo de texto, tem que passar o encode também
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    verificaPalavrasDuplicadas(texto);
+})
+
+// criar um array com as palavras
+// contar as ocorrências
+// montar um objeto com o resultado
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1
+    });
+    console.log(resultado);
+}
